23a269201dfd3eb1f44d376374f32156c7f8becb
Surya Handika Putratama <ubunteroz@gmail.com>
2013-08-12 10:04:47 +0700

    FIX: fix path of ign-sudoers.ign

--- test/ign-sudoers.ign/install.sh
+++ test/ign-sudoers.ign/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'Pastikan Anda Telah Memasang IGNSDK' 
 cp bin/* /usr/sbin/
-mkdir -p  /usr/share/ign-sdk/test/IGN-sudoers/
-cp -rf ../IGN-sudoers/* /usr/share/ign-sdk/test/IGN-sudoers/
-chmod 644 /usr/share/ign-sdk/test/IGN-sudoers/logo.png
-cp  -rf ign-sudoers.desktop /usr/share/applications/
+mkdir -p  /usr/share/ign-sdk/test/ign-sudoers.ign/
+cp -rf ../ign-sudoers.ign/* /usr/share/ign-sdk/test/ign-sudoers.ign/
+chmod 644 /usr/share/ign-sdk/test/ign-sudoers.ign/logo.png
+cp  -rf ignsdk-ignsudoers.desktop /usr/share/applications/
 echo 'Selesai';
 exit 0

572ce5854b0dac630df45d2b7e115db9a8d94dda
wajatmaka <wajatmaka@gmail.com>
2013-08-14 07:00:56 +0700

    Perubahan direktori projek ignsudoers /opt

--- test/ign-sudoers.ign/install.sh
+++ test/ign-sudoers.ign/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'Pastikan Anda Telah Memasang IGNSDK' 
 cp bin/* /usr/sbin/
-mkdir -p  /usr/share/ign-sdk/test/ign-sudoers.ign/
-cp -rf ../ign-sudoers.ign/* /usr/share/ign-sdk/test/ign-sudoers.ign/
-chmod 644 /usr/share/ign-sdk/test/ign-sudoers.ign/logo.png
+mkdir -p  /opt/ignsdk/ign-sudoers.ign/
+cp -rf ../ign-sudoers.ign/* /opt/ignsdk/ign-sudoers.ign/
+chmod 644 /opt/ignsdk/ign-sudoers.ign/logo.png
 cp  -rf ignsdk-ignsudoers.desktop /usr/share/applications/
 echo 'Selesai';
 exit 0

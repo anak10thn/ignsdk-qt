25d2c37f666e90b5b8bf7f663b02e19e45d871ed
wajatmaka <wajatmaka@gmail.com>
2013-08-15 19:51:17 +0700

    Pembenahan direktori

--- test/ign-sudoers.ign/uninstall.sh
+++ test/ign-sudoers.ign/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf /usr/sbin/ign-sudo*
-rm -rf  /opt/ignsdk/ign-sudoers/
+rm -rf  /usr/share/ign-sdk/ign-sudoers.ign
 rm  -rf /usr/share/applications/ign-sudoers.desktop 
 echo 'Terhapus';
 exit 0

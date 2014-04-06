89e41f646d19070a26313c64ecc974996c06a824
anak10thn <linux@toroo.org>
2014-04-07 00:56:44 +0700

    ADD: new API set(proxy) issue #124

--- ignnetwork.cpp
+++ ignnetwork.cpp
@@ -21,3 +21,56 @@ QString ignnetwork::myIP(){
         return host;
     }
 }
+
+void ignnetwork::setProxy(const QVariant &config){
+    /* json parsing */
+    QJsonParseError *err = new QJsonParseError();
+    QJsonDocument json = QJsonDocument::fromVariant(config);
+
+    if (err->error != 0) {
+      qDebug() << err->errorString();
+      exit (1);
+    }
+
+    QJsonObject jObject = json.object();
+    QVariantMap set_proxy = jObject.toVariantMap();
+    if(set_proxy["type"].toString() != ""){
+        QNetworkProxy proxy;
+        QString proxy_type = set_proxy["type"].toString();
+        if(proxy_type == "http"){
+            proxy.setType(QNetworkProxy::HttpProxy);
+        }
+        else if(proxy_type == "socks5"){
+            proxy.setType(QNetworkProxy::Socks5Proxy);
+        }
+        else if(proxy_type == "ftp"){
+            proxy.setType(QNetworkProxy::FtpCachingProxy);
+        }
+        else if(proxy_type == "httpCaching"){
+            proxy.setType(QNetworkProxy::HttpCachingProxy);
+        }
+        else{
+            qDebug()<<"Please input your type proxy (http,socks5,ftp,httpCaching)!";
+        }
+
+        if(set_proxy["url"].toString() != ""){
+            QString url = set_proxy["url"].toString();
+            QStringList url_proxy = url.split(":");
+            proxy.setHostName(url_proxy.at(0));
+            proxy.setPort(url_proxy.at(1).toInt());
+        }
+        else{
+            qDebug()<<"Please input your hostname:port Ex: 127.0.0.1:8080!";
+        }
+
+        if(set_proxy["username"].toString() != ""){
+            proxy.setUser(set_proxy["username"].toString());
+        }
+
+        if(set_proxy["password"].toString() != ""){
+            proxy.setPassword(set_proxy["password"].toString());
+        }
+
+        QNetworkProxy::setApplicationProxy(proxy);
+    }
+}
--- ignnetwork.h
+++ ignnetwork.h
@@ -3,6 +3,11 @@
 
 #include <QObject>
 #include <QNetworkInterface>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonParseError>
+#include <QNetworkProxy>
 
 class ignnetwork : public QObject
 {
@@ -11,6 +16,7 @@ public:
     explicit ignnetwork(QObject *parent = 0);
 public slots:
     QString myIP();
+    void setProxy(const QVariant &config);
 };
 
 #endif // IGNNETWORK_H

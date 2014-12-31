958b98e293b3b4bc9f0f2cad980aec067bb51978
Surya Handika Putratama <ubunteroz@gmail.com>
2014-12-31 13:41:29 +0700

    main.cpp: Use QCommandLineParser (Qt 5.2+).

--- main.cpp
+++ main.cpp
@@ -6,92 +6,48 @@
 #include <iostream>
 #include <getopt.h>
 #include "version.h"
+#include <QCommandLineParser>
 using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     ign w;
-    static int verbose_flag;
-    char *url = NULL;
-    char *optional = NULL;
-    bool dev = false;
+
+    QString url = NULL;
     bool file = false;
-    int index;
-    int c;
-    int r;
-    bool version = false;
-    char help[] = "Usage: ignsdk -p [PROJECT DIRECTORY]\n\nGeneral Options :\n-v\t--version\t\toutput version information and exit\n-d\t--dev\t\t\tWeb Inspector Enable\n-r\t--dev-remote [port]\tRemote debugging\n\nWindow options :\n--transparent\tTransparent Mode\n--noFrame\tFrame Disable\n";
-    opterr = 0;
-    static struct option long_options[] =
-                 {
-                   {"verbose", no_argument,       &verbose_flag, 1},
-                   {"brief",   no_argument,       &verbose_flag, 0},
-                   {"dev",     no_argument,       0, 'd'},
-                   {"file",     required_argument,       0, 'f'},
-                   {"transparent",  no_argument,       0, 't'},
-                   {"noFrame",  no_argument,       0, 'n'},
-                   {"help",  no_argument,       0, 'h'},
-                   {"project",      required_argument, 0, 'p'},
-                   {"dev-remote",      required_argument, 0, 'r'},
-                   {"version",  no_argument,       0, 'v'},
-                   {0, 0, 0, 0}
-                 };
+    QString optional;
 
-               int option_index = 0;
+    QCommandLineParser cmd_parser;
+    cmd_parser.setApplicationDescription("IGOS Nusantara Software Development Kit");
 
-           while ((c = getopt_long (argc, argv, "vdnhf:tp:r:",long_options,&option_index)) != -1){
-             switch (c)
-               {
-               case 'p':
-                 url = optarg;
-                 break;
-               case 'd':
-                 dev = true;
-                  w.setDev(dev);
-                 break;
-               case 'f':
-                file = true;
-                optional = optarg;
-               break;
-               case 'r':
-                r = atoi(optarg);
-                w.setDevRemote(r);
-               break;
-               case 'n':
-                 w.widgetNoFrame();
-               break;
-               case 't':
-                 w.widgetTransparent();
-               break;
-               case 'v':
-                 version = true;
-               break;
-               case 'h':
-                 printf("%s\n",help);
-                 exit(0);
-               break;
-               case '?':
-                 if (optopt == 'c')
-                   fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-                 else if (isprint (optopt))
-                   fprintf (stderr, "Unknown option `-%c'.\n\nUsage: ignsdk -p [PROJECT DIRECTORY]\n\nGeneral Options :\n-v\t--version\t\toutput version information and exit\n-d\t--dev\t\t\tWeb Inspector Enable\n-r\t--dev-remote [port]\tRemote debugging\n\nWindow options :\n--transparent\tTransparent Mode\n--noFrame\tFrame Disable\n", optopt);
-                 else
-                   fprintf (stderr,
-                            "Unknown option character `\\x%x'.\n",
-                            optopt);
-                 return 1;
-               default:
-                 abort ();
-               }
-           }
+    QCommandLineOption cmd_project(QStringList() << "p" << "project", "Specify project directory", "directory");
+    cmd_parser.addOption(cmd_project);
+    QCommandLineOption cmd_file(QStringList() << "f" << "file", "Load specific HTML file", "file");
+    cmd_parser.addOption(cmd_file);
+    QCommandLineOption cmd_dev(QStringList() << "d" << "development", "Activate development mode");
+    cmd_parser.addOption(cmd_dev);
+    QCommandLineOption cmd_remote(QStringList() << "r" << "remote", "Activate remote debugging", "port");
+    cmd_parser.addOption(cmd_remote);
+    QCommandLineOption cmd_version(QStringList() << "v" << "version", "Show version");
+    cmd_parser.addOption(cmd_version);
+    cmd_parser.addHelpOption();
 
-    for (index = optind; index < argc; index++){
-       printf ("Non-option argument %s\n", argv[index]);
+    cmd_parser.process(a);
+
+    if (cmd_parser.isSet(cmd_version)) {
+      printf("IGNSDK version %s (%s). Compiled on %s %s. Maintained by %s.\n", IGNSDK_VERSION, IGNSDK_CODENAME, __DATE__, __TIME__, IGNSDK_MAINTAINER);
+      exit(0);
     }
 
-    if(version){
-        printf("IGNSDK version %s (%s). Compiled on %s %s. Maintained by %s.\n", IGNSDK_VERSION, IGNSDK_CODENAME, __DATE__, __TIME__, IGNSDK_MAINTAINER);
-        exit(0);
+    url = cmd_parser.value(cmd_project);
+
+    if (cmd_parser.isSet(cmd_remote)) {
+      w.setDevRemote(cmd_parser.value(cmd_remote).toInt());
+    }
+
+    if (cmd_parser.isSet(cmd_file)) {
+      file = true;
+      optional = cmd_parser.value(cmd_file);
     }
 
     QString opt = url;
